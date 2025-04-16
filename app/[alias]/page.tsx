afb7b91eae6cd1d0e63eaf88bb64000d28a32c3b
Show530 <sophiahowson@gmail.com>
2025-04-16 16:49:36 -0400

    changed try catch logic

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -3,23 +3,23 @@ import {redirect} from "next/navigation"
 
 export default async function RedirectionPage({params, }: {params: Promise<{aliasFromUrl: string}>}) {
     const {aliasFromUrl} = await params;
+    let redirectUrl = "/"
 
     try {
         // const url = await
         const currAlias = await getUrlFromAlias(aliasFromUrl);
 
-        if (currAlias == null) {
-            redirect(`/error`);
-        }
-        else {
+        if (currAlias != null) {
             console.log(currAlias);
             console.log('Alias is: ',  currAlias.alias);
             console.log('URL is: ', currAlias.url);
-            return redirect(currAlias.url);
+            // return redirect(currAlias.url);
+            redirectUrl = currAlias.url;
         }
+
     } catch(err) {
         console.error(err);
-        return redirect("/");
+        // return redirect("/");
     }
-
+    redirect(redirectUrl);
 }
