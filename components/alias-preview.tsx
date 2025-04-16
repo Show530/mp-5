fcabbda544a7d9c08236379f3201b3912f04e9a5
Show530 <sophiahowson@gmail.com>
2025-04-16 16:18:57 -0400

    fixed outputted go to url to be with vercel link

--- components/alias-preview.tsx
+++ components/alias-preview.tsx
@@ -5,7 +5,7 @@ export default function AliasPreview({alias}:{alias:AliasProps}) {
         <div>
             <h4 className="font-bold text-3xl">You have used: {alias.url}</h4>
             <h4 className="font-bold text-3xl">To create the alias: {alias.alias}</h4>
-            <h4 className="font-bold text-3xl">To access this url, go to link vercel/{alias.alias}</h4>
+            <h4 className="font-bold text-3xl">To access this url, go to https://mp-5-six.vercel.app/{alias.alias}</h4>
         </div>
     );
 }
